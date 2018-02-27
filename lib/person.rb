1770d180a7245b207f86df537c48eb130bf0b1ca
Neil Seejoor <nvseejoor@gmail.com>
2018-02-27 02:06:58 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,9 @@
 class Person
   #your code here
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-                :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(hash)
+    hash.each {|k, v| self.send("#{key=}",value)}          
 end
